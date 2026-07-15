95d482a0edbe332d6994015d77699374e8ab4bd3
chiranjib-swain <chiranjib-swain@github.com>
2026-07-15 12:36:54 +0530

    chore: refactor Jest configuration and update tsconfig.json for ESM compatibility

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,22 @@
-import {createDefaultEsmPreset, type JestConfigWithTsJest} from 'ts-jest';
-
-const preset = createDefaultEsmPreset({
-  tsconfig: './tsconfig.json'
-});
-
-const config: JestConfigWithTsJest = {
-  ...preset,
+export default {
   clearMocks: true,
-  moduleFileExtensions: ['js', 'mjs', 'cjs', 'ts', 'json', 'node'],
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        diagnostics: {
+          ignoreCodes: [151002]
+        }
+      }
+    ]
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: ['node_modules/(?!(@actions|@octokit)/)'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   },
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts', '**/*.spec.ts'],
   verbose: true
 };
-
-export default config;
